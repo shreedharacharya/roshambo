426e53d06e44fac8bd888402dc7c9d7f46b10d69
TJ Karagosian <timkaragosian@gmail.com>
2019-01-11 20:56:15 -0500

    added behavior to button presses

--- app/src/main/java/com/timkaragosian/roshambo/MainActivity.java
+++ app/src/main/java/com/timkaragosian/roshambo/MainActivity.java
@@ -2,6 +2,8 @@ package com.timkaragosian.roshambo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainMenuFragment mainMenuFragment = new MainMenuFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_activity_layout, mainMenuFragment, mainMenuFragment.getTag())
+                .commit();
+
+        initViews();
+        initListeners();
+    }
+
+    public void initViews(){
+
+    }
+
+    public void initListeners(){
+
     }
 }
--- app/src/main/java/com/timkaragosian/roshambo/MainMenuFragment.java
+++ app/src/main/java/com/timkaragosian/roshambo/MainMenuFragment.java
@@ -2,28 +2,74 @@ package com.timkaragosian.roshambo;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class MainMenuFragment extends Fragment {
 
+    Button mPlayGame;
+    Button mRules;
+    Button mQuit;
 
     public MainMenuFragment() {
-        // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_menu, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+        initListeners();
+    }
+
+    public void initViews() {
+        if (getView() != null) {
+            mPlayGame = getView().findViewById(R.id.play_game_button);
+            mRules = getView().findViewById(R.id.rules_button);
+            mQuit = getView().findViewById(R.id.quit_button);
+        }
+    }
+
+    public void initListeners() {
+        mPlayGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //have it begin game
+            }
+        });
+
+        mRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RulesFragment rulesFragment = new RulesFragment();
+
+                if (getActivity() != null && getActivity().getSupportFragmentManager() != null) {
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_activity_layout, rulesFragment, rulesFragment.getTag())
+                            .commit();
+                }
+            }
+        });
+
+        mQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() != null) {
+                    getActivity().finish();
+                }
+            }
+        });
+    }
 }
